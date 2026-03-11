b09b97e52f3d4cbe101f5d77a4c3c19452ab34a6
Herbert Silva Pacheco <cvmsp.marketing@gmail.com>
2026-03-11 19:17:36 +0000

    firebase-studio-1772718769747.cluster-hkcruqmgzbd2aqcdnktmz6k7ba.cloudwo

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -13,6 +13,7 @@ const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => {
+  // Destructure internal Radix props to avoid passing them to the DOM
   const { onInteractOutside, onPointerDownOutside, ...domProps } = props as any;
 
   return (
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -71,6 +71,7 @@ const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
 >(({ className, children, position = "popper", ...props }, ref) => {
+  // Destructure custom props to avoid passing them to the DOM
   const { onInteractOutside, onPointerDownOutside, ...domProps } = props as any;
 
   return (
