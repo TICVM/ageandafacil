aa62c03cf0391117b2cdac018d5583dcbfcde18f
Herbert Silva Pacheco <cvmsp.marketing@gmail.com>
2026-03-19 13:49:37 +0000

    react-dom-client.development.js:25630 Download the React DevTools for a

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -83,7 +82,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "relative z-[300] max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md",
           "data-[state=open]:animate-in data-[state=closed]:animate-out",
-          "data-[state=closed]:fade-out-80 data-[state=open]:fade-in-0",
+          "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
           "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
           "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1",
           "data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
@@ -94,7 +93,6 @@ const SelectContent = React.forwardRef<
         {...cleanProps}
       >
         <SelectScrollUpButton />
-
         <SelectPrimitive.Viewport
           className={cn(
             "p-1",
@@ -104,7 +102,6 @@ const SelectContent = React.forwardRef<
         >
           {children}
         </SelectPrimitive.Viewport>
-
         <SelectScrollDownButton />
       </SelectPrimitive.Content>
     </SelectPrimitive.Portal>
