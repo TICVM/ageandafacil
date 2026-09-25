ee8bc7349eb7bea6d880f2eaaffcbd50d4bcabb2
Herbert Silva Pacheco <cvmspinfo@gmail.com>
2026-09-25 13:52:43 -0300

    Import AppHome component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Label } from '@/components/ui/label';
 import { Segment, Class } from '@/lib/types';
+import { AppHome } from "@/components/AppHome";
 
 export default function LoginPage() {
   const router = useRouter();
